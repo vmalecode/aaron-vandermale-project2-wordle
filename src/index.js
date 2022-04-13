cd8f79b1b99a02ffe842fc40f191a4ba8e24b6a2
Aaron <aaron.vmale@gmail.com>
2022-04-12 18:37:35 -0700

    add persistance

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import allReducer from './reducers';
-import { addLetter, keyPress, incCount } from './actions';
-import store from './store';
-
-// const store = createStore(allReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ &&window.__REDUX_DEVTOOLS_EXTENSION__());
-
-// document.addEventListener('keydown', logKey);
+import store, {persistor} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 
 ReactDOM.render(
-  <Provider store = {store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
+    <Provider store = {store}>
+      <PersistGate persistor = {persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -2,6 +2,15 @@ import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import allReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+const persistConfig = {
+    key: 'persist-key',
+    storage
+}
+const persistedReducer = persistReducer(persistConfig, allReducer);
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-const store = createStore(allReducer, composedEnhancer)
+const store = createStore(persistedReducer, composedEnhancer)
+const persistor = persistStore(store);
 export default store
+export {persistor};
